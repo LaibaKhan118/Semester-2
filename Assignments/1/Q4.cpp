3dff3c6d696d382978a178ecb304c5cefee1d084
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2025-03-16 00:04:38 -0700

    Update Q4.cpp

--- Assignments/1/Q4.cpp
+++ Assignments/1/Q4.cpp
@@ -3,13 +3,16 @@ using namespace std;
 
 class Route
 {
-public:
+private:
     const string routes[5][3] = {
-        {"Hyderi", "Sakhi", "Five"},
+        {"Hyderi", "SakhiHassan", "FiveStar"},
         {"Johar", "Continental", "Millennium"},
-        {"Bahadurabad", "Char", "Numaish"},
-        {"Saddar", "Burns", "Teen"},
+        {"Bahadurabad", "Gulshan", "Numaish"},
+        {"Saddar", "BurnsRd", "TeenTalwar"},
         {"Defence", "Gizri", "Dolmen"}};
+
+public:
+    const string (*getRoutes() const)[3] { return routes; }
 };
 
 class Student
@@ -17,51 +20,41 @@ class Student
 private:
     string name;
     string address;
-
-public:
-    Route *r;
     int id;
     bool paymentStatus;
     int pointNumber;
+    Route *route;
     static int studentCount;
 
-    void setname(string name)
-    {
-        this->name = name;
-    }
-    string getname()
-    {
-        return name;
-    }
-    void setadd(string address)
-    {
-        this->address = address;
-    }
-    string getadd()
-    {
-        return address;
-    }
+public:
+    string getName() const { return name; }
+    string getAddress() const { return address; }
+    int getId() const { return id; }
+    bool getPaymentStatus() const { return paymentStatus; }
+    int getPointNumber() const { return pointNumber; }
+    static int getStudentCount() { return studentCount; }
 
-    Student()
-    {
-        id = 0;
-        name = "";
-        address = "";
-        paymentStatus = false;
-    }
+    void setName(const string &newName) { name = newName; }
+    void setAddress(const string &newAddress) { address = newAddress; }
+    void setId(int newId) { id = newId; }
+    void setPaymentStatus(bool newStatus) { paymentStatus = newStatus; }
+    void setPointNumber(int newPointNumber) { pointNumber = newPointNumber; }
 
-    Student(int id, string name, string address, bool paymentStatus, Route &r)
-        : id(id), name(name), address(address), paymentStatus(paymentStatus), r(&r) {}
+    Student() : id(0), name(""), address(""), paymentStatus(false), pointNumber(0), route(nullptr) {}
 
-    void registerForTransportationAndPayFee(Route &r)
+    Student(int id, const string &name, const string &address, bool paymentStatus, Route &route)
+        : id(id), name(name), address(address), paymentStatus(paymentStatus), route(&route), pointNumber(0) {}
+
+    void registerForTransportationAndPayFee(Route &route)
     {
-        int choice;
-        bool found = 0;
+        bool found = false;
+        const string(*routes)[3] = route.getRoutes();
+
         for (int i = 0; i < 5; i++)
         {
             for (int j = 0; j < 3; j++)
             {
-                if (address == r.routes[i][j])
+                if (address == routes[i][j])
                 {
                     if (paymentStatus)
                     {
@@ -69,13 +62,14 @@ public:
                         cout << "You are registered. Your route number is " << pointNumber << endl;
                         cout << "You will be dropped off at Fast University in the morning and picked up in the evening." << endl;
                         studentCount++;
-                        found = 1;
+                        found = true;
                         return;
                     }
                     else
                     {
                         cout << "Payment required for registration!" << endl;
-                        found = 1;
+                        found = true;
+                        int choice;
                         cout << "Do you want to pay now? (1 for yes, 0 for no)" << endl;
                         cin >> choice;
                         if (choice == 1)
@@ -102,41 +96,47 @@ public:
         }
     }
 
-    void studentDetails()
+    void studentDetails() const
     {
         cout << endl
              << "---Student Details---" << endl;
         cout << "Student ID: " << id << endl;
         cout << "Student Name: " << name << endl;
         cout << "Address: " << address << endl;
-        cout << "Payment Status: " << (paymentStatus ? "Paid" : "Not Paid");
+        cout << "Payment Status: " << (paymentStatus ? "Paid" : "Not Paid") << endl;
         cout << "Point Number: " << pointNumber << endl;
     }
 };
 
 class Admin
 {
-public:
+private:
     int id;
     string name;
     string password;
-    int response;
     static int adminCount;
 
-    Admin()
+public:
+    int getId() const { return id; }
+    string getName() const { return name; }
+    static int getAdminCount() { return adminCount; }
+
+    void setId(int newId) { id = newId; }
+    void setName(const string &newName) { name = newName; }
+    void setPassword(const string &newPassword) { password = newPassword; }
+
+    Admin() : id(0), name(""), password("")
     {
-        id = 0;
-        name = "";
-        password = "";
+        adminCount++;
     }
 
-    Admin(int id, string name, string password)
+    Admin(int id, const string &name, const string &password)
         : id(id), name(name), password(password)
     {
         adminCount++;
     }
 
-    void adminDetails()
+    void adminDetails() const
     {
         cout << endl
              << "---Admin Details---" << endl;
@@ -144,22 +144,23 @@ public:
         cout << "Admin Name: " << name << endl;
     }
 
-    void attendance(Student &s)
+    void attendance(Student &student) const
     {
         cout << endl
              << "---Attendance---" << endl;
-        cout << s.getname() << " show you card please." << endl;
+        cout << student.getName() << ", show your card please." << endl;
         cout << "Choose response: " << endl
              << "    1. Here is my card" << endl
              << "    2. I don't have my card" << endl;
+        int response;
         cin >> response;
         if (response == 1)
         {
-            cout << "Your attendance is marked " << s.getname() << endl;
+            cout << "Your attendance is marked, " << student.getName() << endl;
         }
         else if (response == 2)
         {
-            cout << "Your attendance will not be marked " << s.getname() << endl;
+            cout << "Your attendance will not be marked, " << student.getName() << endl;
         }
     }
 };
@@ -169,11 +170,11 @@ int Admin::adminCount = 0;
 
 int main()
 {
-    Route r;
-    Student s;
-    Admin a;
+    Route route;
+    Student student;
+    Admin admin;
 
-    int x;
+    int choice;
     cout << "\t---FAST Transportation System---" << endl;
     do
     {
@@ -182,15 +183,14 @@ int main()
              << "    2. Admin" << endl
              << "    3. Exit" << endl
              << "Select an option: ";
-        cin >> x;
+        cin >> choice;
 
-        string address;
-        string name;
-        bool paymentStatus;
-        int id;
-
-        if (x == 1)
+        if (choice == 1)
         {
+            string name, address;
+            int id;
+            bool paymentStatus;
+
             cout << "----Transportation System----" << endl;
             cout << "Enter your name: ";
             cin >> name;
@@ -200,26 +200,29 @@ int main()
                  << "    (0 for unpaid and 1 for paid): ";
             cin >> paymentStatus;
             cout << "We provide transport service in the following areas: " << endl;
-            int num = 1;
+            const string(*routes)[3] = route.getRoutes();
             for (int i = 0; i < 5; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    cout << num << ". " << r.routes[i][j] << endl;
-                    num++;
+                    cout << (i * 3 + j + 1) << ". " << routes[i][j] << endl;
                 }
             }
             cout << "Enter your area name: ";
             cin >> address;
-            s.setname(name);
-            s.setadd(address);
-            s = Student(id, name, address, paymentStatus, r);
-            s.registerForTransportationAndPayFee(r);
-        }
 
-        else if (x == 2)
+            student.setName(name);
+            student.setAddress(address);
+            student.setId(id);
+            student.setPaymentStatus(paymentStatus);
+            student.registerForTransportationAndPayFee(route);
+            student.studentDetails();
+        }
+        else if (choice == 2)
         {
-            string password;
+            string name, password;
+            int id;
+
             cout << endl
                  << "----Admin Login----" << endl;
             cout << "Enter your name: ";
@@ -228,14 +231,17 @@ int main()
             cin >> id;
             cout << "Enter password: ";
             cin >> password;
-            a = Admin(id, name, password);
-            a.adminDetails();
-            a.attendance(s);
+
+            admin.setName(name);
+            admin.setId(id);
+            admin.setPassword(password);
+            admin.adminDetails();
+            admin.attendance(student);
         }
-        else if (x == 3)
+        else if (choice == 3)
         {
-            cout << "Number of registered students: " << Student::studentCount << endl;
-            cout << "Number of admins: " << Admin::adminCount << endl;
+            cout << "Number of registered students: " << Student::getStudentCount() << endl;
+            cout << "Number of admins: " << Admin::getAdminCount() << endl;
             cout << "Exiting the system..." << endl;
         }
         else
@@ -243,7 +249,14 @@ int main()
             cout << "Invalid choice. Please try again." << endl;
         }
 
-    } while (x != 3);
+    } while (choice != 3);
 
     return 0;
 }
+/*
+Things That Were Wrong In The Previous Code:
+1. No Encapsulation
+2. Inflexible Data
+3. Input Validation
+4. No proper use of static members
+*/
