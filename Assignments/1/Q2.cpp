0b2bf22a2e71c3ecfdccbb1b72a6afda07ebdf2f
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2025-03-15 23:16:31 -0700

    Update Q2.cpp

--- Assignments/1/Q2.cpp
+++ Assignments/1/Q2.cpp
@@ -2,53 +2,16 @@
 #include <tuple>
 using namespace std;
 
-class Robot
+class Ball
 {
-public:
-    string name;
-    int hits;
+private:
     int x, y;
 
-    Robot(string n)
+public:
+    Ball()
     {
         x = 0;
         y = 0;
-        hits = 0;
-        name = n;
-    }
-
-    void hitBall(int &ballX, int &ballY, const string &direction)
-    {
-        if (direction == "up")
-        {
-            ballY += 1;
-            y += 1;
-        }
-        else if (direction == "down")
-        {
-            ballY -= 1;
-            y -= 1;
-        }
-        else if (direction == "left")
-        {
-            ballX -= 1;
-            x -= 1;
-        }
-        else if (direction == "right")
-        {
-            ballX += 1;
-            x += 1;
-        }
-        hits++;
-    }
-
-    int getX()
-    {
-        return x;
-    }
-    int getY()
-    {
-        return y;
     }
 
     void move(int dx, int dy)
@@ -57,73 +20,133 @@ public:
         y += dy;
     }
 
-    tuple<int, int> getPosition()
+    tuple<int, int> getPosition() const
     {
         return make_tuple(x, y);
     }
 
-    bool isGoalReached(int ballX, int ballY)
+    bool isGoalReached(int goalX, int goalY) const
     {
-        if (ballX == 3 && ballY == 3)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return (x == goalX && y == goalY);
     }
+
+    int getX() const { return x; }
+    int getY() const { return y; }
+
+    void setX(int newX) { x = newX; }
+    void setY(int newY) { y = newY; }
+};
+
+class Goal
+{
+private:
+    int x, y;
+
+public:
+    Goal(int goalX, int goalY)
+    {
+        x = goalX;
+        y = goalY;
+    }
+
+    bool isGoalReached(int ballX, int ballY) const
+    {
+        return (ballX == x && ballY == y);
+    }
+
+    int getX() const { return x; }
+    int getY() const { return y; }
+
+    void setX(int newX) { x = newX; }
+    void setY(int newY) { y = newY; }
+};
+
+class Robot
+{
+private:
+    string name;
+    int hits;
+
+public:
+    Robot(string n)
+    {
+        hits = 0;
+        name = n;
+    }
+
+    void hitBall(Ball &ball, const string &direction)
+    {
+        if (direction == "up")
+        {
+            ball.move(0, 1);
+        }
+        else if (direction == "down")
+        {
+            ball.move(0, -1);
+        }
+        else if (direction == "left")
+        {
+            ball.move(-1, 0);
+        }
+        else if (direction == "right")
+        {
+            ball.move(1, 0);
+        }
+        hits++;
+    }
+    string getName() const { return name; }
+    int getHits() const { return hits; }
+
+    void setName(string newName) { name = newName; }
+    void setHits(int newHits) { hits = newHits; }
 };
 
 class Team
 {
-public:
+private:
     string name;
     Robot *rob;
 
-    Team()
-    {
-        name = "";
-    }
-
+public:
     Team(string n, Robot &rob)
     {
         name = n;
         this->rob = &rob;
     }
+
+    string getName() const { return name; }
+    Robot *getRobot() const { return rob; }
+
+    void setName(string newName) { name = newName; }
+    void setRobot(Robot *newRobot) { rob = newRobot; }
 };
 
 class Game
 {
-public:
+private:
     Team team1, team2;
-    int ballX, ballY;
+    Ball ball;
+    Goal goal;
 
-    Game(string n1, string n2, Robot &rob1, Robot &rob2)
+public:
+    Game(string n1, string n2, Robot &rob1, Robot &rob2, int goalX, int goalY)
+        : team1(n1, rob1), team2(n2, rob2), goal(goalX, goalY)
     {
-        team1 = Team(n1, rob1);
-        team2 = Team(n2, rob2);
-        ballX = 0;
-        ballY = 0;
+        ball = Ball();
     }
 
     void startGame()
     {
         cout << "Game started" << endl;
-        team1.rob->x = 0;
-        team1.rob->y = 0;
-        team2.rob->x = 0;
-        team2.rob->y = 0;
+        ball = Ball();
     }
 
     void play(Team &team)
     {
         char a;
-        ballX = 0;
-        ballY = 0;
-        cout << team.name << "'s turn!" << endl;
+        cout << team.getName() << "'s turn!" << endl;
         cout << "A: Left  W: Up  S: Down  D: Right" << endl;
-        while (!team.rob->isGoalReached(ballX, ballY))
+        while (!goal.isGoalReached(ball.getX(), ball.getY()))
         {
             cout << "Play your move: " << endl;
             cin >> a;
@@ -131,44 +154,44 @@ public:
             {
             case 'A':
             case 'a':
-                team.rob->hitBall(ballX, ballY, "left");
+                team.getRobot()->hitBall(ball, "left");
                 break;
             case 'W':
             case 'w':
-                team.rob->hitBall(ballX, ballY, "up");
+                team.getRobot()->hitBall(ball, "up");
                 break;
             case 'S':
             case 's':
-                team.rob->hitBall(ballX, ballY, "down");
+                team.getRobot()->hitBall(ball, "down");
                 break;
             case 'D':
             case 'd':
-                team.rob->hitBall(ballX, ballY, "right");
+                team.getRobot()->hitBall(ball, "right");
                 break;
             default:
                 cout << "Invalid move! Try again." << endl;
                 continue;
             }
-            cout << team.rob->name << " moved ball to (" << ballX << ", " << ballY << ")" << endl;
+            cout << team.getRobot()->getName() << " moved ball to (" << ball.getX() << ", " << ball.getY() << ")" << endl;
         }
-        cout << team.name << " reached the goal in " << team.rob->hits << " hits!" << endl;
+        cout << team.getName() << " reached the goal in " << team.getRobot()->getHits() << " hits!" << endl;
     }
 
     void declareWinner()
     {
         cout << endl
              << "----Results----" << endl;
-        cout << team1.name << " scored in " << team1.rob->hits << " hits" << endl
+        cout << team1.getName() << " scored in " << team1.getRobot()->getHits() << " hits" << endl
              << "And" << endl;
-        cout << team2.name << " scored in " << team2.rob->hits << " hits" << endl
+        cout << team2.getName() << " scored in " << team2.getRobot()->getHits() << " hits" << endl
              << endl;
-        if (team1.rob->hits < team2.rob->hits)
+        if (team1.getRobot()->getHits() < team2.getRobot()->getHits())
         {
-            cout << "Team " << team1.name << " wins" << endl;
+            cout << "Team " << team1.getName() << " wins" << endl;
         }
-        else if (team2.rob->hits < team1.rob->hits)
+        else if (team2.getRobot()->getHits() < team1.getRobot()->getHits())
         {
-            cout << "Team " << team2.name << " wins" << endl;
+            cout << "Team " << team2.getName() << " wins" << endl;
         }
         else
         {
@@ -182,11 +205,19 @@ int main()
     Robot r1("Galaxy Annihilator");
     Robot r2("Astroid Destroyer");
 
-    Game g("Hunters", "Gladiators", r1, r2);
+    Game g("Hunters", "Gladiators", r1, r2, 3, 3);
     g.startGame();
-    g.play(g.team1);
-    g.play(g.team2);
+    g.play(g.getTeam1());
+    g.startGame();
+    g.play(g.getTeam2());
     g.declareWinner();
 
     return 0;
 }
+/*
+Mistakes in the previous Code:
+1. No Ball class. It's not the Robot's responsibility to manage the Ball's position.
+2. No Goal class. It's not the Robot's responsibility to handle the Goal.
+3. No encapsulation
+4. No reset after first player's done
+*/
