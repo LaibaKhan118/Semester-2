bfc36e1917e10fd4dc44997cce45bcb46c85f6c8
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2025-03-17 09:58:54 +0500

    Create Q2.cpp

---
+++ Lab_7/Q2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Shape
+{
+protected:
+    float posX;
+    float posY;
+    string clr;
+    float Borderthickness;
+};
+
+int main()
+{
+
+    return 0;
+}
