ec2cbb8d3fc2136bf607a5d06e3c8b6182f12e78
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2025-04-15 14:59:47 +0500

    Create Library.cpp

---
+++ Lab_9/Q3/Library.cpp
@@ -0,0 +1,56 @@
+/*• Class Library (Optional for advanced students):
+o Data Members:
+▪ A collection (e.g., array/list) of Book objects.
+o Member Functions:
+▪ addBook(), removeBook(), searchBook(): Manage the collection.
+*/
+
+#include "Book.h"
+#include "Library.h"
+#include <iostream>
+using namespace std;
+
+Library::Library(int max) : maxBooks(max), bookCount(0) {
+            books = new Book*[maxBooks];
+        }
+
+void Library::addBook(const Book& book) {
+   if (bookCount < maxBooks) {
+        books[bookCount] = book;
+        bookCount++;
+        cout << "Book added successfully!" << endl;
+    }
+    else {
+        cout << "Cannot add book. Library is full!" << endl;
+}
+
+void Library::removeBook() {
+    int index;
+    cout << "Enter index of book to remove: ";
+    cin >> index;
+
+    if (index < 0 || index >= bookCount) {
+        cout << "Invalid index!" << endl;
+        return;
+    }
+
+    delete books[index];
+    for (int i = index; i < bookCount - 1; i++) {
+        books[i] = books[i + 1];
+    }
+    bookCount--;
+}
+
+void Library::searchBook() {
+    string title;
+    cout << "Enter book title to search: ";
+    cin >> title;
+
+    for (int i = 0; i < bookCount; i++) {
+        if (books[i]->getTitle() == title) {
+            cout << "Book found: " << books[i]->getTitle() << ", " << books[i]->getAuthor() << ", " << books[i]->getISBN() << endl;
+            return;
+        }
+    }
+    cout << "Book not found!" << endl;
+}
